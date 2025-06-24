7101168b5bc6c3c57a77a2751832f76fab7c9d1e
zonarr <poomison2013@gmail.com>
2025-06-24 17:59:11 +0700

    Add data description function and integrate it into main workflow

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import da
+
 def main():
-	data = pd.read_csv("train.csv")
+    df = pd.read_csv("train.csv")
+    da.describe_data(df)
+    age = df["Age"]
+    age_fare = df["Age","Fare"]
+    age_sorted = age.sort_values()
 
 if __name__ == "__main__":
 	main()
